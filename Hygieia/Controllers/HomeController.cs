b2dfa9beb2ec61d58c49edc98fa5f271d0e27c2e
Ignars3 <ignars3@gmail.com>
2022-05-01 00:07:06 +0300

    Created new Web Api project

---
+++ Hygieia/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Hygieia.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Login()
+        {
+            return "Login";
+        }
+    }
+}
